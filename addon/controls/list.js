727983804e2933695e154f1f2aad5d1686b0f1f5
Eric van Blokland <eric@footsteps.nl>
2015-07-16 13:22:50 +0200

    Fixed list control update issue
    _loadItemControls should observe both value and value.@each

--- addon/controls/list.js
+++ addon/controls/list.js
@@ -42,7 +42,7 @@ export default Control.extend(ControlSupport,{
 		return this._itemControls;
 	}),
 	
-	_loadItemControls : Ember.observer('value.@each',function() {
+	_loadItemControls : Ember.observer('value,value.@each',function() {
 		var control=this;
 		var value=this.get('value');
 		var oldControls=this._itemControls;
