916e283799659744006a5a0d680ef410629c0a50
Eric van Blokland <eric@footsteps.nl>
2015-08-05 10:26:00 +0200

    Fixed delayed for loading in a form via form helper
    Control should initialize with "for" set, instead of waiting for the
    component to initialize.

--- addon/helpers/form.js
+++ addon/helpers/form.js
@@ -13,8 +13,10 @@ import Lookup from 'furnace-forms/utils/lookup-class';
  * @return {String} HTML string  
  */
 export default function formHelper(params, hash, options, env) {
+
 	Ember.assert("The for attribute of a form expects a object reference, not a string literal",hash['for']===undefined || (typeof hash['for']==='object' && hash['for'].isStream===true));
-	var contextObject=hash['for'] ? hash['for'].value() : null;
+	var forStream=hash['for'];
+	var contextObject=forStream ? forStream.value() : null;
 	Ember.assert("You are required to specify a form or the 'for' attribute",params[0] || contextObject!==undefined);
 	var view = env.data.view;
 	if(!contextObject) {
@@ -32,7 +34,21 @@ export default function formHelper(params, hash, options, env) {
 		named=view.get('renderedName');
 	}
 	var controlClass=Lookup.call(view,named ? named :contextObject);
-	var control=controlClass.create({container:view.container,target:view.get('controller')});
+	var control=controlClass.createWithMixins({container:view.container,target:view.get('controller'),
+		'for' : Ember.computed({
+			get : function() {
+				return forStream.value();
+			},
+			set : function(key,value) {
+				return value;
+			}
+		})
+	});
+	
+	var forStreamSubscriber = forStream.subscribe(function() {
+		this.notifyPropertyChange('for');
+	},control);
+	
 	var component=control.getComponent();
 	options.helperName='form';
 	hash.control=control;	
@@ -40,8 +56,12 @@ export default function formHelper(params, hash, options, env) {
 		destroy : function() {
 			this._super();
 			this.control.destroy();
+			forStreamSubscriber();
 		}
 	});
 	Ember.assert('Control ('+control._name+') does not specify a component',component);
+	
+	delete hash['for'];
+	
 	return env.helpers.view.helperFunction.call(this,[component],hash,options,env);
 }
