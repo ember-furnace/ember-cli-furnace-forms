7252bf50d50255aaaaeb89f0ca40b9e224853cfe
Eric van Blokland <eric@footsteps.nl>
2015-10-28 12:05:21 +0100

    Fixed issue with conditional initialization

--- addon/mixins/controls/conditional.js
+++ addon/mixins/controls/conditional.js
@@ -9,23 +9,26 @@ export default Ember.Mixin.create({
 		if(typeof this._conditionFn==='function') {
 			// Do not schedule this, causes all kinds of syncing issues
 			this.reopen({
-				_condition:Ember.computed(this._conditionProps+',_form.for',this._conditionFn)
+				_conditionObserver:Ember.observer(this._conditionProps+',_form.for',function() {
+					this.set('_condition',this._conditionFn());
+				}).on('init')
 			});
 			
 			
 		}
 	},
 	
-	// Initialize the condition so observers get notified and our condition will observe its condition properties
-	_initCondition : Ember.on('init',function() {
-		this.get('hasPrerequisites');
-	}),
+//	// Initialize the condition so observers get notified and our condition will observe its condition properties
+//	_initCondition : Ember.on('init',function() {
+//		console.log('init condition '+this,this.get('hasPrerequisites'));
+//	}),
 	
 	hasPrerequisites : Ember.computed.alias('_condition'),
 
 	conditionProperties: Ember.computed('_conditionProps',function() {		
-		if(!this._conditionProps)
+		if(!this._conditionProps) {
 			return null;
+		}
 		return Ember.A(this._conditionProps.split(','));
 	}).readOnly().volatile(),
 });
