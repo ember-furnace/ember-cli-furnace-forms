ember-furnace/ember-cli-furnace-forms

commit e03d42f596ef903808c46a51d162bded5484af14

Refactored "for" attribute
Internals now work primarily with the private _model attribute
Several regressions fixed

addon/controls/abstract.js
@@ -135,8 +135,9 @@ export default Ember.Object.extend(Ember.ActionHandler,{
 	_didReset : false,
 	
 	_reset: function() {
-		this.setFlag('isValid',null);
-		this.set('_valid',null);
+// This breaks already set validation, our value might not change by a reset so may not trigger a new validation		
+//		this.setFlag('isValid',null);
+//		this.set('_valid',null);
 		this._didReset=true;		
 	},
 	
@@ -151,21 +152,24 @@ export default Ember.Object.extend(Ember.ActionHandler,{
 	_getPath: function() {
 		// Temporary work arround to give lists a proper path
 		// Temporary work arround to make sure forms dont get an absolute path
-		if(!this['for'] || this._isList || (this.get('for')!==this.get('_panel.for') && !this._isForm)) {
+		if(!this['_model'] || this._isList || (this.get('_model')!==this.get('_panel._model') && !this._isForm)) {
 			return (this.get('_panel._path') ? this.get('_panel._path')+ "." : '')+this.get('_name');
-		} else if(this._panel && this.get('for')===this.get('_panel.for')) {
+		} else if(this._panel && this.get('_model')===this.get('_panel._model')) {
 			return this.get('_panel._path');
 		}	
 
 	},
 	
-	getFor : function(path) {
+	getFor : function(path) {		
 		var model=null;
-		if(this['for'])
-			model = this.get('for');			
-		else
-			model = this.getForm().get('for');
-		if(!model) {			
+		if(this['_model'])
+			model = this.get('_model');			
+		else {
+			if(this.getForm()) {
+				model = this.getForm().get('_model');
+			}
+		}
+		if(!model) {		
 			Ember.warn('Control '+this.toString()+' is trying to access the (form)model but it is not defined!',model);
 			return undefined;
 		}

addon/controls/form.js
@@ -215,7 +215,7 @@ var Form = Panel.extend({
 //			return this._form.get("_validator."+this._path);
 //		}		
 		return this.constructor.typeKey;
-	}.property('for'),
+	}.property('_model'),
 	
 	validationDetached : Ember.computed('isEnabled,_validationDetached',function() {
 		return !this.get('isEnabled') || this.get('_validationDetached');
@@ -225,7 +225,7 @@ var Form = Panel.extend({
 	
 	_validate : function(paths) {
 		var form=this;
-		var target=this.get('for');			
+		var target=this.get('_model');			
 		var modelName=this.get('_modelName');
 		var validator=this.get('_validator');
 		var promisses=Ember.A();
@@ -251,7 +251,7 @@ var Form = Panel.extend({
 				if(input) {
 					paths.removeObject(path);
 					validator=input.get('_form._validator');
-					target=input.get('_form.for');
+					target=input.get('_form._model');
 					path=input.get('_path');
 					if(path) {
 						Ember.assert('No target for validation of path ' + path + " in form " +form,target);
@@ -331,6 +331,8 @@ var Form = Panel.extend({
 	
 	'for' : null,
 	
+	_model : Ember.computed.alias('for'),
+	
 	init : function() {
 		this._forms=Ember.A();
 		this._controlsByPath={};
@@ -338,70 +340,75 @@ var Form = Panel.extend({
 		this._messageCache={};
 		
 		if(!this._syncFromSource || !this._syncToSource) {
-			var _for=this.get('for') ;
 			this.reopen({
-				'for' : Ember.computed(function(key,value) {
-							if(value) {
-								if(!this._syncFromSource || !this._syncToSource) {
-									return Proxy.create({_content:value,
-										_syncFromSource: this._syncFromSource,
-										_syncToSource: this._syncToSource});
-								}
-								return value;						
-							}
-							return null;
-						}),
+				_asyncObserver : Ember.observer('for',function() {
+					var model=this.get('for');
+					if(this.get('_model') instanceof Proxy) {
+						this.get('_model').destroy();
+					}
+					if(model) {
+						if(!this._syncFromSource || !this._syncToSource) {							
+							model= Proxy.create({_content:model,
+								_syncFromSource: this._syncFromSource,
+								_syncToSource: this._syncToSource});
+						}										
+					}						
+					this.set('_model',model);
+				}),
+				_model : null,
 			});
-			this.set('for',_for);
+			this._asyncObserver();
 		} 
 		
 		this._super();
 		this.set('_path',null);		
 		
 //		var targetObject=this._syncToSource ? this['for'] : this.get('_proxy');
-		Ember.run.scheduleOnce('afterRender',this,function() {
-			var validator=this.get('_validator');
-			// Initialize validaton if a validator was resolved and we're the root form, or the root form validator has no validation for our path
-			if(validator && (!this._form || !(this._form.get("_validator."+this._path) instanceof validator.constructor))) {
-				var form= this;
-				this._observer = validator.observe(this,'for',function(result,sender,key) {
-					var silent=sender===null || sender===form;
-					// If our target model changed, immediate show validations unless it's a new model
-					if(silent && ((form.get('for.isDirty')===true || (form.get('for.isDirty')===undefined && form.get('isDirty'))) && !form.get('for.isNew'))) {
-						silent=false;
-					}
-//					
-//					Ember.debug(form);
-//					console.log('Ran validation',sender ? sender.toString() : null,key,silent);
-//					console.log(result.get('validations'));
-//					console.log(result.get('messages'));
-//					Ember.debug(sender);
-//					console.log('--------------');
-					
-					form._setValidations(result,silent);
-					
-					// We only want to receive changed status on next run, so reset the result if it has finished
-					if(result.hasFinished()) {
-						result.reset();
-					}
-				}, this.get('_modelName'));
-				
-			
-				if(this.get('for')) {
-					this._currentFor=this.get('for');
-					this._observer.run();
-				}
-			}
-		});
+		Ember.run.scheduleOnce('afterRender',this,this._setupValidator);
 	},
 	
-	_currentFor : undefined,
+	_setupValidator : function() {
+		var validator=this.get('_validator');
+		// Initialize validaton if a validator was resolved and we're the root form, or the root form validator has no validation for our path
+		if(validator && (!this._form || !(this._form.get("_validator."+this._path) instanceof validator.constructor))) {
+			var form= this;
+			this._observer = validator.observe(this,'_model',function(result,sender,key) {
+				var silent=sender===null || sender===form;
+				// If our target model changed, immediate show validations unless it's a new model
+				if(silent && ((form.get('_model.isDirty')===true || (form.get('_model.isDirty')===undefined && form.get('isDirty'))) && !form.get('_model.isNew'))) {
+					silent=false;
+				}
+//				
+//				Ember.debug(form);
+//				console.log('Ran validation',sender ? sender.toString() : null,key,silent);
+//				console.log(result.get('validations'));
+//				console.log(result.get('messages'));
+//				Ember.debug(sender);
+//				console.log('--------------');
+				
+				form._setValidations(result,silent);
+				
+				// We only want to receive changed status on next run, so reset the result if it has finished
+				if(result.hasFinished()) {
+					result.reset();
+				}
+			}, this.get('_modelName'));
+			
+		
+			if(this.get('_model')) {
+				this._currentModel=this.get('_model');
+				this._observer.run();
+			}
+		}
+	},
 	
-	_forObserver : Ember.observer('for',function(){
+	_currentModel : undefined,
+	
+	_modelObserver : Ember.observer('_model',function(){
 		// Only reset if 'for' actually changed
-		if(this.get('for')!==this._currentFor) {
+		if(this.get('_model')!==this._currentModel) {
 			this._reset(true);
-			this._currentFor=this.get('for');
+			this._currentModel=this.get('_model');
 		}
 	}),
 	

addon/controls/list.js
@@ -105,7 +105,7 @@ export default Control.extend(ControlSupport,{
 	controls: Ember.computed.union('_controls','_itemControls').readOnly(),
 	
 	// We alias the for property for panels and forms
-	'for' : Ember.computed.alias('value'),
+	'_model' : Ember.computed.alias('value'),
 	
 	_controlDirtyObserver: Ember.observer('_controls.@each.isDirty,_itemControls.@each.isDirty',function(){		
 		this._super();

addon/controls/panel.js
@@ -24,21 +24,11 @@ export default Control.extend(ControlSupport,{
 	
 	'for' : null,
 	
+	_model : Ember.computed.alias('for'),
+	
 	modelName: Ember.computed.alias('_modelName'),
 	
 	init: function() {
-//		if(this['for']===null) {
-//			if(this.get('_panel.for.'+this._name)) {
-//				this.reopen({
-//					'for': Ember.computed.alias('_panel.for.'+this._name)
-//				});
-//			} else {
-//				this.reopen({
-//					'for': Ember.computed.alias('_panel.for')
-//				});
-//			}
-//		}
-		
 		this._super();
 	},
 	
@@ -64,15 +54,15 @@ export default Control.extend(ControlSupport,{
 	
 
 	
-	_modelName : Ember.computed('for',function(key,value) {
+	_modelName : Ember.computed('_model',function(key,value) {
 		if(value)  {
 			return value;
 		}	
-		if(this.get('for')===this.get('_panel.for')) {
+		if(this.get('_model')===this.get('_panel._model')) {
 			return this.get('_panel._modelName');
 		}
-		else if(this.get('for')) {
-			var name= getName(this.get('for'),true);
+		else if(this.get('_model')) {
+			var name= getName(this.get('_model'),true);
 			if(name)
 				return name;
 			return this.get('_panel._modelName')+'.'+this._name;
@@ -96,8 +86,8 @@ export default Control.extend(ControlSupport,{
 	_apply:function() {
 		this._super();
 		this.get('inputControls').invoke('_apply');
-		if((!this._form || this.get('for')!==this._form.get('for')) && this.get('for') instanceof Proxy) {
-			this.get('for')._apply();
+		if((!this._form || this.get('_model')!==this._form.get('_model')) && this.get('_model') instanceof Proxy) {
+			this.get('_model')._apply();
 		}
 	},
 	
@@ -128,22 +118,22 @@ export default Control.extend(ControlSupport,{
 //				options['for']=Ember.computed.alias('_panel.for');
 //			} else
 			
-			if(options._name!==null && Ember.Enumerable.detect(options._panel.get('for'))) {
-				if(options._panel.get('for') instanceof DS.ManyArray) {
-					options['for']=Ember.computed('_panel.for',function() {
-						if(!this.get('_panel.for')) {
+			if(options._name!==null && Ember.Enumerable.detect(options._panel.get('_model'))) {
+				if(options._panel.get('_model') instanceof DS.ManyArray) {
+					options['for']=Ember.computed('_panel._model',function() {
+						if(!this.get('_panel._model')) {
 							return undefined;
 						}
-						return this.get('_panel.for').objectAt(this._name);
+						return this.get('_panel._model').objectAt(this._name);
 					});
 				} else {
-					options['for']=Ember.computed.alias('_panel.for.'+options._name);
+					options['for']=Ember.computed.alias('_panel._model.'+options._name);
 				}
 			}
-			else if(options._panel.get('for') && options._panel.get('for.'+options._name)!==undefined) {
-				options['for']=Ember.computed.alias('_panel.for.'+options._name);
+			else if(options._panel.get('_model') && options._panel.get('_model.'+options._name)!==undefined) {
+				options['for']=Ember.computed.alias('_panel._model.'+options._name);
 			} else {
-				options['for']=Ember.computed.alias('_panel.for');						
+				options['for']=Ember.computed.alias('_panel._model');						
 			}
 		}
 		mixins.push(options);

addon/controls/view.js
@@ -27,11 +27,11 @@ export default Control.extend({
 		if(this.get('_name')!==null && this.value===undefined) {
 			var propertyName='_panel.for.'+this.get('_name');
 			
-			var property=this.get('_panel.for.');
+			var property=this.get('_panel._model.');
 			if(property instanceof DS.ManyArray) {
 				this.reopen({
-					value: Ember.computed('_panel.for,_panel.for.@each',function() {
-						return this.get('_panel.for').objectAt(this._name);
+					value: Ember.computed('_panel._model,_panel._model.@each',function() {
+						return this.get('_panel._model').objectAt(this._name);
 					})
 				});
 			}

addon/inputs/list/check-option.js
@@ -30,7 +30,7 @@ export default Input.extend(CheckedSupport,{
 	click : function(event) {
 		var target=event.toElement || event.target;
 		if(target.id===this.get('inputId')) {
-			this.send('select');
+			this.control.select();
 		}
 	}
 });

addon/inputs/list/radio-option.js
@@ -31,7 +31,7 @@ export default Input.extend(CheckedSupport,{
 	click : function(event) {
 		var target=event.toElement || event.target;
 		if(target.id===this.get('inputId')) {
-			this.send('select');
+			this.control.select();
 		}
 	}
 });

addon/mixins/controls/conditional.js
@@ -9,7 +9,7 @@ export default Ember.Mixin.create({
 		if(typeof this._conditionFn==='function') {
 			// Do not schedule this, causes all kinds of syncing issues
 			this.reopen({
-				_conditionObserver:Ember.observer(this._conditionProps+',_form.for',function() {
+				_conditionObserver:Ember.observer(this._conditionProps+',_form._model',function() {
 					this.set('_condition',this._conditionFn());
 				}).on('init')
 			});

commit 9dd7e7e7f6c30e4b6a337f3c5f83fad7bad86882

Fixed issue with stuck validation on model reset.
Broken by e03d42f596ef903808c46a51d162bded5484af14

addon/controls/abstract.js
@@ -139,10 +139,12 @@ export default Ember.Object.extend(Ember.ActionHandler,{
 	
 	_didReset : false,
 	
-	_reset: function() {
-// This breaks already set validation, our value might not change by a reset so may not trigger a new validation		
-//		this.setFlag('isValid',null);
-//		this.set('_valid',null);
+	_reset: function(modelChanged) {
+// This breaks already set validation, our value might not change by a reset so may not trigger a new validation
+		if(modelChanged) {
+			this.setFlag('isValid',null);
+			this.set('_valid',null);
+		}
 		this._didReset=true;		
 	},
 	