7b643bd9b64753728fea704289073af38fa962d5
Eric van Blokland <eric@footsteps.nl>
2016-07-12 14:21:20 +0200

    Added url validator

---
+++ app/validators/local/url.js
@@ -0,0 +1,16 @@
+import Validation from 'furnace-validation';
+export default Validation.Property.extend({
+	
+	match: "^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u{00a1}-\u{ffff}0-9]+-?)*[a-z\u{00a1}-\u{ffff}0-9]+)(?:\.(?:[a-z\u{00a1}-\u{ffff}0-9]+-?)*[a-z\u{00a1}-\u{ffff}0-9]+)*(?:\.(?:[a-z\u{00a1}-\u{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$",
+	
+	_regexp: null,
+	
+	init: function() {
+		this._regexp=new RegExp(this.match);
+	},
+	call:function(context,value,result) {
+		if(value && !this._regexp.test(value)) {
+			result.addError(context,'invalidUrl',[],'delayed');
+		}
+	}
+})
