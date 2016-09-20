34cf10d336716773e62f217212ad13cad4327daa
Eric van Blokland <eric@footsteps.nl>
2016-09-20 15:38:37 +0200

    Number mixin no longer allows null and undefined by default
    Caused issues with several implementations

--- addon/mixins/controls/number.js
+++ addon/mixins/controls/number.js
@@ -11,6 +11,7 @@ export default Ember.Mixin.create({
 	
 	precision : 0,
 	
+	canBeNull: false,
 	
 	actions : {
 		stepUp : function() {
@@ -21,47 +22,57 @@ export default Ember.Mixin.create({
 		}
 	},
 	
+	init : function() {
+		this._super();
+		if(!this._checkValue()) {			
+			this._valueObserver();
+		}
+	},
+	
 	_checkValue: function() {
 		var value=this.get('value');
-		if(isNaN(value)) {
-			if(this.get('min')!==null) {
-				this.set('value',this.get('min'));
-			} else {
-				this.set('value',0);
+		if(value===null && this.canBeNull) {
+			return true;
+		}
+		if(typeof value!=='number') {
+			value=Number(value);
+			if(isNaN(value)) {
+				if(this.canBeNull) {
+					value=null;
+				} else {
+					value = this.get('min')!==null ? this.get('min') : 0;
+				}
 			}
-			return;
+			this.set('value',value);
+			return false;
 		}
 		if(this.get('min')!==null && value<this.get('min')) {
-			this.set('value',this.get('min'));				
+			this.set('value',this.get('min'));
+			return false;
 		}
 		if(this.get('max')!==null && value>this.get('max')) {
-			this.set('value',this.get('max'));				
+			this.set('value',this.get('max'));
+			return false;
 		}
 		switch(this.get('real')) {
 			case true:
 				if(this.value<0) {
 					this.set('value',0);
+					return false;
 				}
 				break;
 			case false:
 				if(this.value>0) {
 					this.set('value',0);
+					return false;
 				}
 				break;
 		}
+		return true;
 	},
 	
 	_constraintObserver : Ember.observer('min,max,real',function() {
 		this._checkValue();
-	}),
-	
-	value : Ember.computed({
-		set:function(key,value) {
-			if(typeof value!=='number' && value!==null && value!==undefined) {				
-				return Number(value);
-			}
-			return value;
-		},
 	}),
 	
 	_valueObserver : Ember.observer('value',function() {
