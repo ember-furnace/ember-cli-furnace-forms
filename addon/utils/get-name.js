2cd05487ecb95b1e5c901ad81bcd8a4e9e13a414
Eric van Blokland <eric@footsteps.nl>
2017-02-22 13:40:53 +0100

    Updated get-name to get form name for a model behind a proxy

--- addon/utils/get-name.js
+++ addon/utils/get-name.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 import Panel from 'furnace-forms/controls/panel';
+import { ProxyMixin } from 'furnace-forms/proxy';
+
+function getObjectName(object) {
+	var objectName=null;
+	if(object.constructor.modelName!==undefined) {
+		objectName=object.constructor.modelName;
+	} else if(object.constructor.typeKey!==undefined) {
+		objectName=object.constructor.typeKey;
+	} else {
+		var tmpName = object.constructor.toString();
+		var index=tmpName.indexOf(':');
+		objectName=tmpName.substring(index+1,tmpName.indexOf(':',index+1)).replace(/\//g,'.');
+	}
+	return objectName;
+}
+
 export default function(object,silent) {
 	var objectName=null;
 	if(typeof object === 'string') {
@@ -14,15 +30,17 @@ export default function(object,silent) {
 		objectName=tmpName.substring(index+1,tmpName.indexOf(':',index+1)).replace(/\//g,'.');	
 	} else if(object instanceof Panel) {
 		objectName=object.get('layoutName');
-	} else if(object instanceof Ember.Component) {		
-		objectName=object.get('layoutName');
+	} else if(object instanceof Ember.Component) {
+		objectName=object.get('layoutName').replace(/\//g,'.');
 	} else if(object instanceof Ember.Object) {
-		if(object.constructor.modelName!==undefined) {
-			objectName=object.constructor.modelName;
-		} else {
-			objectName=object.constructor.typeKey;
+		// FIXME: We might want to punch through a proxy proxying another proxy
+		if(ProxyMixin.detect(object) && object.get('_content')) {
+			objectName=getObjectName(object.get('_content'));
 		}
-	} 
+		if(objectName===null) {
+			objectName=getObjectName(object);
+		}
+	}
 //	else {
 //		Ember.warn('Can not determine form for type '+(typeof object));
 //		console.trace();
